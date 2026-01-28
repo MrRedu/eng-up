a3dab3ce787a70f75d9eadcdcd6e06757a8c49cd
Eduardo <ejrb1234@gmail.com>
2026-01-28 17:39:36 -0400

    add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,23 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import {
+  // Geist,
+  Geist_Mono,
+  Sora,
+} from 'next/font/google';
 import { Providers } from '@/components/Providers';
 import { AppSidebar } from '@/components/AppSidebar';
 import { Header } from '@/components/Header';
 import { Search } from '@/components/Search';
 import { SidebarInset } from '@/components/ui/sidebar';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+// const geistSans = Geist({
+//   variable: '--font-geist-sans',
+//   subsets: ['latin'],
+// });
+
+const soraSans = Sora({
+  variable: '--font-sora-sans',
   subsets: ['latin'],
 });
 
@@ -22,9 +31,9 @@ export const metadata: Metadata = {
   description:
     'A virtual backup for my English notes, tracking my progress and topics learned over 5 months.',
   keywords: ['English', 'Learning', 'Grammar', 'Notes', 'Backup'],
-  authors: [],
-  creator: '',
-  publisher: '',
+  authors: [{ name: 'Eduardo R.', url: 'https://github.com/MrRedu' }],
+  creator: 'Eduardo R.',
+  publisher: 'Eduardo R.',
   openGraph: undefined,
   twitter: undefined,
 };
@@ -37,12 +46,12 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
+        className={`${soraSans.variable} ${geistMono.variable} antialiased font-sans`}
       >
         <Providers>
           <AppSidebar />
           <SidebarInset className="bg-sidebar">
-            <div className="relative bg-background mx-2 my-2 border shadow-sm rounded-xl flex flex-col flex-1 min-h-screen">
+            <div className="relative bg-background mx-2 my-2 border shadow-sm rounded-xl flex flex-col flex-1">
               <Header />
               <main className="flex-1 p-6 md:p-8">{children}</main>
             </div>
