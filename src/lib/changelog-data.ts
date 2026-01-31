24d3d1e9c43013a5813a9f31f47a8a8f2ffe7ad4
Eduardo <ejrb1234@gmail.com>
2026-01-30 20:53:36 -0400

    Added "There is / There are" and "Much / Many" lessons with initial content and SEO metadata.

--- src/lib/changelog-data.ts
+++ src/lib/changelog-data.ts
@@ -8,6 +8,14 @@ export interface ChangelogEntry {
 
 export const CHANGELOG_DATA: ChangelogEntry[] = [
   {
+    version: 'v1.9.2',
+    title: 'New Basic Topics',
+    description:
+      'Added "There is / There are" and "Much / Many" lessons with initial content and SEO metadata.',
+    date: '2026-01-30 00:00:00',
+    type: 'feat',
+  },
+  {
     version: 'v1.12.0',
     title: 'Layout Overflow Fix',
     description: 'Fixed layout overflow.',
